633b407830df2d427f9f06d3b267144b6b70c72c
Travis Miller <milletr@tulsaschools.org>
2023-02-24 12:17:54 -0600

    Add Personal Info template

--- src/script.ts
+++ src/script.ts
@@ -45,6 +45,8 @@ async function runReport(browser: Browser, student_number: number) {
   const transcript_pdf_path = outFilePath(`${student_number}-transcript.pdf`)
   const testscores_html_path = outFilePath(`${student_number}-testscores.html`)
   const testscores_pdf_path = outFilePath(`${student_number}-testscores.pdf`)
+  const personalinfo_html_path = outFilePath(`${student_number}-personalinfo.html`)
+  const personalinfo_pdf_path = outFilePath(`${student_number}-personalinfo.pdf`)
   const merged_pdf_path = outFilePath(`${student_number}.pdf`)
 
   try {
@@ -86,6 +88,12 @@ async function runReport(browser: Browser, student_number: number) {
       admin,
     }))
 
+    // console.info('Rendering Personal Info HTML…')
+    fs.writeFile(personalinfo_html_path, render('personalinfo.njk', {
+      date: REPORT_DATE,
+      student_data_transcript,
+    }))
+
     // console.info('Generating Transcript PDF…')
     await report.pdf(browser, transcript_html_path, {
       path: transcript_pdf_path,
@@ -114,10 +122,24 @@ async function runReport(browser: Browser, student_number: number) {
       }
     })
 
+    // console.info('Generating Personal Info PDF…')
+    await report.pdf(browser, personalinfo_html_path, {
+      path: personalinfo_pdf_path,
+      format: 'letter',
+      printBackground: true,
+      margin: {
+        bottom: '0.25in',
+        left: '0.25in',
+        right: '0.25in',
+        top: '0.25in',
+      }
+    })
+
     // console.info('Merging PDFs…')
     const merger = new PDFMerger()
     await merger.add(transcript_pdf_path)
     await merger.add(testscores_pdf_path)
+    await merger.add(personalinfo_pdf_path)
     await merger.save(merged_pdf_path)
   } catch (error) {
     await Promise.allSettled([
