50139ae83591e951a414140274f5f0a41f154306
Jason Bennefield <benneja@tulsaschools.org>
2023-03-06 17:58:20 -0600

    use page 5 to test output of itbs,pbp,sat,sta9

--- src/script.ts
+++ src/script.ts
@@ -9,27 +9,27 @@ import fastq from 'fastq'
 import CliProgress from 'cli-progress'
 
 import { render } from '~/templates'
-import  { queries, prisma } from '~/queries'
+import { queries, prisma } from '~/queries'
 import { launchBrowser, closeBrowser } from '~/browser'
 
 const errors: Error[] = []
 
 type Task = {
-  browser: Browser,
-  student_number: number,
+  browser: Browser
+  student_number: number
 }
 
 type StudentNumberFilter = number[]
 
 const q: queueAsPromised<Task> = fastq.promise(asyncWorker, 20)
 
-async function asyncWorker({browser, student_number}: Task): Promise<void> {
+async function asyncWorker({ browser, student_number }: Task): Promise<void> {
   await runReport(browser, student_number)
 }
 
 const REPORT_DATE = new Date()
 const OUT_DIRECTORY = path.join(__dirname, '..', 'out')
-const outFilePath = (filename:string) => path.join(OUT_DIRECTORY, filename)
+const outFilePath = (filename: string) => path.join(OUT_DIRECTORY, filename)
 const progress = new CliProgress.SingleBar({})
 
 const classHistReducer = (prev: any, curr: any): any => {
@@ -39,25 +39,35 @@ const classHistReducer = (prev: any, curr: any): any => {
 }
 
 async function runReport(browser: Browser, student_number: number) {
-
   const transcript_html_path = outFilePath(`${student_number}-transcript.html`)
   const transcript_pdf_path = outFilePath(`${student_number}-transcript.pdf`)
   const testscores_html_path = outFilePath(`${student_number}-testscores.html`)
   const testscores_pdf_path = outFilePath(`${student_number}-testscores.pdf`)
-  const personalinfo_html_path = outFilePath(`${student_number}-personalinfo.html`)
-  const personalinfo_pdf_path = outFilePath(`${student_number}-personalinfo.pdf`)
+  const personalinfo_html_path = outFilePath(
+    `${student_number}-personalinfo.html`
+  )
+  const personalinfo_pdf_path = outFilePath(
+    `${student_number}-personalinfo.pdf`
+  )
   const merged_pdf_path = outFilePath(`${student_number}.pdf`)
 
   try {
     // queries
-    const student_data_transcript = await queries.studentDataTranscript(student_number)
+    const student_data_transcript = await queries.studentDataTranscript(
+      student_number
+    )
     const student_data_tests = await queries.studentDataTests(student_number)
     const ecum = await queries.ecum(student_number)
     const demo = await queries.demo(student_number)
     const misc = await queries.misc(student_number)
     const years_grade = await queries.yearsGrade(student_number)
-    const fall_class_hst = (await queries.fallClassHst(student_number)).reduce(classHistReducer, {})
-    const spring_class_hst = (await queries.springClassHst(student_number)).reduce(classHistReducer, {})
+    const fall_class_hst = (await queries.fallClassHst(student_number)).reduce(
+      classHistReducer,
+      {}
+    )
+    const spring_class_hst = (
+      await queries.springClassHst(student_number)
+    ).reduce(classHistReducer, {})
     const elem_grd = await queries.elemGrd(student_number)
     const attend = await queries.attend(student_number)
     const mobility = await queries.mobility(student_number)
@@ -88,43 +98,62 @@ async function runReport(browser: Browser, student_number: number) {
     // queries testscores - ACT
     const act = await queries.act(student_number)
 
+    const pbp = await queries.pbp(student_number)
+    const sta9 = await queries.sat9(student_number)
+    const sat = await queries.sat(student_number)
+    const itbs = await queries.itbs(student_number)
+    // const nagl = await queries.nagl(student_number)
+
     // html transcript
-    fs.writeFile(transcript_html_path, render('transcript.njk', {
-      date: REPORT_DATE,
-      student_data_transcript,
-      student_data_tests,
-      ecum,
-      demo,
-      misc,
-      years_grade,
-      fall_class_hst,
-      spring_class_hst,
-      elem_grd,
-      attend,
-      mobility,
-      address_history,
-    }))
+    fs.writeFile(
+      transcript_html_path,
+      render('transcript.njk', {
+        date: REPORT_DATE,
+        student_data_transcript,
+        student_data_tests,
+        ecum,
+        demo,
+        misc,
+        years_grade,
+        fall_class_hst,
+        spring_class_hst,
+        elem_grd,
+        attend,
+        mobility,
+        address_history,
+      })
+    )
 
     // html testscores
-    fs.writeFile(testscores_html_path, render('testscores.njk', {
-      date: REPORT_DATE,
-      student_data_transcript,
-      admin,
-      otis,
-      iowa_new,
-      oklacore,
-      gates,
-      explore,
-      eoi,
-      plan,
-      act
-    }))
+    fs.writeFile(
+      testscores_html_path,
+      render('testscores.njk', {
+        date: REPORT_DATE,
+        student_data_transcript,
+        admin,
+        otis,
+        iowa_new,
+        oklacore,
+        gates,
+        explore,
+        eoi,
+        plan,
+        act,
+        pbp,
+        sta9,
+        sat,
+        itbs,
+      })
+    )
 
     // html personalinfo
-    fs.writeFile(personalinfo_html_path, render('personalinfo.njk', {
-      date: REPORT_DATE,
-      student_data_transcript,
-    }))
+    fs.writeFile(
+      personalinfo_html_path,
+      render('personalinfo.njk', {
+        date: REPORT_DATE,
+        student_data_transcript,
+      })
+    )
 
     // pdf transcript
     await report.pdf(browser, transcript_html_path, {
@@ -137,7 +166,7 @@ async function runReport(browser: Browser, student_number: number) {
         left: '1in',
         right: '1in',
         top: '0.25in',
-      }
+      },
     })
 
     // pdf testscores
@@ -151,7 +180,7 @@ async function runReport(browser: Browser, student_number: number) {
         left: '1in',
         right: '1in',
         top: '0.25in',
-      }
+      },
     })
 
     // pdf personalinfo
@@ -164,7 +193,7 @@ async function runReport(browser: Browser, student_number: number) {
         left: '0.25in',
         right: '0.25in',
         top: '0.25in',
-      }
+      },
     })
 
     // merge all of the pdf files
@@ -174,31 +203,36 @@ async function runReport(browser: Browser, student_number: number) {
     await merger.add(personalinfo_pdf_path)
     await merger.save(merged_pdf_path)
   } catch (error) {
-    await Promise.allSettled([
-      merged_pdf_path,
-    ].map(fs.unlink))
+    await Promise.allSettled([merged_pdf_path].map(fs.unlink))
     throw error
   } finally {
-    await Promise.allSettled([
-      transcript_html_path,
-      transcript_pdf_path,
-      testscores_html_path,
-      testscores_pdf_path,
-    ].map(fs.unlink))
+    await Promise.allSettled(
+      [
+        transcript_html_path,
+        transcript_pdf_path,
+        testscores_html_path,
+        testscores_pdf_path,
+      ].map(fs.unlink)
+    )
   }
 }
 
-async function runReports(browser: Browser, student_number_filter?: StudentNumberFilter) {
+async function runReports(
+  browser: Browser,
+  student_number_filter?: StudentNumberFilter
+) {
   const student_numbers = (await queries.studentNumbers())
-    .map(o => o.STUDENT_NUMBER)
-    .filter(number => !student_number_filter || student_number_filter.includes(number))
+    .map((o) => o.STUDENT_NUMBER)
+    .filter(
+      (number) =>
+        !student_number_filter || student_number_filter.includes(number)
+    )
   console.info('Student Number Count:', student_numbers.length)
 
   progress.start(student_numbers.length, 0)
 
-  student_numbers.forEach(student_number => {
-    q
-      .push({browser, student_number})
+  student_numbers.forEach((student_number) => {
+    q.push({ browser, student_number })
       .catch((error) => {
         errors.push(error)
         console.error('\nError:', student_number, error)
@@ -209,8 +243,10 @@ async function runReports(browser: Browser, student_number_filter?: StudentNumbe
   await q.drained().then(() => progress.stop())
 }
 
-function studentNumbersFilter(): StudentNumberFilter|undefined {
-  const argv_integers = process.argv.map(Number).filter(n => n % 1 == 0 && n > 0)
+function studentNumbersFilter(): StudentNumberFilter | undefined {
+  const argv_integers = process.argv
+    .map(Number)
+    .filter((n) => n % 1 == 0 && n > 0)
 
   if (argv_integers.length) {
     return argv_integers
@@ -228,15 +264,11 @@ process.on('exit', () => {
   console.info('Closing Browser…')
   const closePromise = closeBrowser()
 
-  return Promise.all([
-    disconnectPromise,
-    closePromise,
-  ])
+  return Promise.all([disconnectPromise, closePromise])
 })
-
 ;(async () => {
   try {
-    await fs.mkdir(OUT_DIRECTORY, {recursive: true})
+    await fs.mkdir(OUT_DIRECTORY, { recursive: true })
     const browser = await launchBrowser()
     await runReports(browser, studentNumbersFilter())
     process.exit(0)
